e01e0f165a9eb6cc07c54d54c4f70a485e8601c0
Simon Taylor <nizmox@gmail.com>
2015-04-10 15:41:30 +1000

    make generateValue more DRY

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
+//perform the migration on the provided doc (returning the modified document)
 var migrator = function(doc, migration) {
   if (migration.target && migration.value) {
     doc[migration.target] = generateValue(doc, migration.value);
   }
-
   if (migration.delete) {
     delete doc[migration.delete];
   }
-
   return doc;
 }
 
+//generate the value needed based on the format provided
 var generateValue = function(doc, format) {
   //if format is an array, map through the array to generate the value
   if (format instanceof Array) {
     return format.map(function(chunk) {
-      if (typeof chunk === "object") return doc[chunk.field];
-      return chunk;
+      return getValue(doc, chunk);
     }).join('');
   //if format is not an array, just generate the value
   } else {
-    if (typeof format === "object") return doc[format.field];
-    return format;
+    return getValue(doc, format);
   }
+}
+
+//get the value from doc if a field value is requested, else return the value itself
+var getValue = function(doc, chunk) {
+  if (typeof chunk === "object") return doc[chunk.field];
+  return chunk;
 }
 
 module.exports = {
